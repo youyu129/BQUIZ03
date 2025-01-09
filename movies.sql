20ebd7445e781ef47c9300d34dee2a3660a40be0
youyu <129youyu@gmail.com>
2025-01-09 15:38:14 +0800

    建立院線片資料表

---
+++ movies.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主機： 127.0.0.1
+-- 產生時間： 2025-01-09 08:36:47
+-- 伺服器版本： 10.4.32-MariaDB
+-- PHP 版本： 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 資料庫： `db19`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `movies`
+--
+
+CREATE TABLE `movies` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` text NOT NULL,
+  `level` int(1) UNSIGNED NOT NULL,
+  `length` int(3) UNSIGNED NOT NULL,
+  `ondate` date NOT NULL,
+  `publish` text NOT NULL,
+  `director` text NOT NULL,
+  `trailer` text NOT NULL,
+  `poster` text NOT NULL,
+  `intro` text NOT NULL,
+  `sh` int(1) UNSIGNED NOT NULL,
+  `rank` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 已傾印資料表的索引
+--
+
+--
+-- 資料表索引 `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
+--
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
