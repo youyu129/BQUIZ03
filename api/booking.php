cd173a31cc1e4cad5516f25574532926f4e3f926
youyu <129youyu@gmail.com>
2025-01-13 16:15:51 +0800

    引入座位圖,並產生20個空座位

--- api/booking.php
+++ api/booking.php
@@ -1,33 +1,51 @@
 <style>
-    #info {
-        width: 540px;
-        height: 370px;
+#info{
+    width:540px;
+    height:370px;
+    margin:auto;
+}
+#movieInfo{
+    width:540px;
+    height:120px;
         margin:auto;
+    background:#ccc;
+    padding:10px 100px;
+}
+
+#info{
+    background-image:url("icon/03D04.png");
+    background-position:center;
+    background-repeat:no-repeat;
+    padding:19px 110px 14px 110px;
+    display:flex;
+    flex-wrap:wrap;
+}
+    .seat{
+    width:64px;
+    height:85px;
+    text-align: center;
+        padding:2px;
     }
-    #movieInfo{
-        width: 540px;
-        height: 100px;
-        margin:auto;
-        background:#ccc;
-        padding:10px 100px;
-    }
+
 </style>
-
-<?php
-include_once "db.php"
-
-
-?>
+<?php  include_once "db.php";?>
 
 <div id="info">
+    <?php
+        for($i=0;$i<20;$i++){
+            echo "<div class='seat'>";
+            echo  floor($i/5)+1 ."排".($i%5+1)."號";
+            echo "</div>";
+        }
+    ?>
 
 </div>
 
 <div id="movieInfo">
     <div>您選擇的電影是：<?=$_GET['name'];?></div>
     <div>您選擇的時刻是：<?=$_GET['date']."&nbsp;&nbsp;".$_GET['session'];?></div>
-    <div>您已經勾選<span id="tickets"></span>張票，最多可以購買四張票</div>
-    <div class="ct">
+    <div>您已經<span id='tickets'></span>勾選張票，最多可以購買四張票</div>
+    <div class='ct'>
         <button onclick="$('#booking,#order').toggle()">上一步</button>
         <button onclick="checkout()">訂購</button>
     </div>
