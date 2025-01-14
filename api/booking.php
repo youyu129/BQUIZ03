6cb2164d4e16b8b39861c109b1fdef912c5783a7
youyu <129youyu@gmail.com>
2025-01-14 09:52:08 +0800

    完成勾選座位功能_完成顯示勾選票數計算_張數可顯示為數字或顯示為國字

--- api/booking.php
+++ api/booking.php
@@ -34,7 +34,7 @@
 .booked{
     background:url("icon/03D03.png") center no-repeat;
 }
-.check{
+.chk{
     position:absolute;
     right:2px;
     bottom:2px;
@@ -48,7 +48,7 @@
         for($i=0;$i<20;$i++){
             echo "<div class='seat null'>";
             echo  floor($i/5)+1 ."排".($i%5+1)."號";
-            echo "<input type='checkbox' class='check' value='$i'>";
+            echo "<input type='checkbox' class='chk' value='$i'>";
             echo "</div>";
         }
     ?>
@@ -58,9 +58,34 @@
 <div id="movieInfo">
     <div>您選擇的電影是：<?=$_GET['name'];?></div>
     <div>您選擇的時刻是：<?=$_GET['date']."&nbsp;&nbsp;".$_GET['session'];?></div>
-    <div>您已經<span id='tickets'></span>勾選張票，最多可以購買四張票</div>
+    <div>您已經勾選<span id='tickets'></span>張票，最多可以購買四張票</div>
     <div class='ct'>
         <button onclick="$('#booking,#order').toggle()">上一步</button>
         <button onclick="checkout()">訂購</button>
     </div>
 </div>
+
+<script>
+let seats=new Array();
+// 數字轉為國字的寫法 建立物件
+// let num={1:'一',2:'二',3:'三',4:'四'};
+
+$(".chk").on("change",function(){
+    if($(this).prop('checked')){
+        if(seats.length>3){
+            alert("最多只能選四張票")
+            $(this).prop('checked',false)
+        }else{
+            seats.push($(this).val())
+        }
+    }else{
+        seats.splice(seats.indexOf($(this).val()),1)
+    }
+    $("#tickets").text(seats.length)
+    
+    // 數字轉為國字的寫法
+    // $("#tickets").text(num[seats.length])
+    
+    // console.log('seats',seats);
+})
+</script>
